7f42e36dbe5f9c3e0e01050906d9ca8c4ea66458
Aamir-kanhoush <aamir.kanhoush2001@gmail,com>
2024-05-19 19:49:13 +0300

    added compare feature

--- X-Ray Bae/WindowsFormsApp1/Form2.Designer.cs
+++ X-Ray Bae/WindowsFormsApp1/Form2.Designer.cs
@@ -34,6 +34,8 @@ namespace WindowsFormsApp1
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -41,7 +43,7 @@ namespace WindowsFormsApp1
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(83, 29);
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(312, 408);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -52,9 +54,10 @@ namespace WindowsFormsApp1
             // pictureBox2
             // 
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox2.Location = new System.Drawing.Point(432, 29);
+            this.pictureBox2.Location = new System.Drawing.Point(423, 0);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(312, 408);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
@@ -63,11 +66,33 @@ namespace WindowsFormsApp1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(12, 414);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(124, 34);
+            this.Button1.TabIndex = 2;
+            this.Button1.Text = "Compare";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(166, 414);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Evaluate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 531);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Button1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form2";
@@ -76,6 +101,10 @@ namespace WindowsFormsApp1
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.Button Button1;
 
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
 
--- X-Ray Bae/WindowsFormsApp1/Form2.cs
+++ X-Ray Bae/WindowsFormsApp1/Form2.cs
@@ -44,5 +44,49 @@ namespace WindowsFormsApp1
                 Image originalImage = Image.FromFile(openFileDialog1.FileName);
                 pictureBox2.Image = originalImage;
             }        }
+
+        private int CountBlackPixels(Bitmap bmp)
+        {
+            int count = 0;
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color pixelColor = bmp.GetPixel(i, j);
+                    if (pixelColor.R == 0 && pixelColor.G == 0 && pixelColor.B == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Bitmap img1 = new Bitmap(pictureBox1.Image);
+            Bitmap img2 = new Bitmap(pictureBox2.Image);
+
+            int blackPixelsImg1 = CountBlackPixels(img1);
+            int blackPixelsImg2 = CountBlackPixels(img2);
+
+            if (Math.Abs(blackPixelsImg1 - blackPixelsImg2) <= 50) // tolerance value
+            {
+                MessageBox.Show("There is no advance in treatment");
+            }
+            else if (blackPixelsImg1 > blackPixelsImg2)
+            {
+                MessageBox.Show("There is advancement in sickness (more sick)");
+            }
+            else
+            {
+                MessageBox.Show("There is advancement in treatment");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                
+        }
     }
 }
